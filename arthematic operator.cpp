49210870bcb91fb4689c5e30ec3e8762e8d44d5f
AkashJavvadi <95211623+AkashJavvadi@users.noreply.github.com>
2021-12-08 13:16:11 +0530

    Add files via upload

---
+++ arthematic operator.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	float a,b,c;
+	printf("enter value for a");
+	scanf("%f",&a);
+	printf("enter value for b");
+	scanf("%f",&b);
+	c=a+b;
+	printf("sum of 2 numbers is %f",c);
+	
+}
